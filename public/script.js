b8bc6338b6533ca14e6a864fbb7d4b540e95e727
Zin Lin Htet <zinlinhtet.devops@gmail.com>
2025-09-10 15:12:08 +0700

    Update: API route path

--- public/script.js
+++ public/script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const priorityInput = document.getElementById('priority-input');
     const categoryInput = document.getElementById('category-input');
     const addTodoBtn = document.getElementById('add-todo-btn');
-    const API_URL = 'https://demo-container.graymushroom-03debb04.southeastasia.azurecontainerapps.io/api/todos';
+    const API_URL = 'http://127.0.0.1:3000';
 
     // Helper function to convert priority number to stars
     const getPriorityStars = (priority) => {
